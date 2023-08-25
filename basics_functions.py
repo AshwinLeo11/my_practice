abd6d0186a5cb3bf8f27f04740eac55e6abdcf0b
AshwinLeo11 <ashwinbalakrishnan11@gmail.com>
2023-08-25 21:02:42 +0530

    basics functions added

---
+++ basics_functions.py
@@ -0,0 +1,110 @@
+# round function excercise :
+
+print(round(7))
+print(type(round(7)))
+
+print(round(7,2))
+print(type(round(7,2)))
+
+print(round(7.6666,2))
+print(type(round(7.6666,2)))
+
+print(round(7.6666))
+print(type(round(7.6666)))
+
+print(round(7.5))
+
+print(round(6.5))
+
+print(round(674,2))
+
+print(round(674,-2))
+
+print(round(674,4))
+
+print(round(-8/3))
+
+print(round(-2.5))
+
+
+# f_strings excercise :
+
+name = 'Ashwin'
+age = 22
+height = 1.7
+print("My name is:"+name,"i am " +str(age),"years old")
+
+print("my name is:", name,"I am",age,"years old", "My height is",height, "meters")
+
+# f_string :
+
+print(f"my name is {name}.I am {age} years old. My height is {height} meters")
+
+print(f"ashwin's father is {age*2} years old.")
+
+
+
+
+# if else  statements excercise :
+
+
+height = int(input("Enter height in feet:"))
+if(height>4):
+    print("Buy token")
+    print("Now you can board the metro")
+else:
+    print("No Token required")
+
+
+
+
+height = int(input("Enter height in feet:"))
+if(height>4):
+    print("Buy token")
+    print("Now you can board the metro")
+print("Out of if block")
+
+
+height = int(input("Enter height in feet:"))
+if height==3:
+    print("Buy token")
+    print("Now you can board the metro")
+else:
+    print("no token required")
+
+# if else coding excercise :
+
+ # problem : check whether given number is even or odd 
+
+number = int(input("Enter the number:"))
+if number % 2 == 0:
+    print('This number is even')
+else:
+    print('This number is odd')
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
