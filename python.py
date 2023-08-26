00b552c061041f62fc10c10e5a8949fa1357aaba
AshwinLeo11 <ashwinbalakrishnan11@gmail.com>
2023-08-26 19:52:27 +0530

    excercise program added

--- python.py
+++ python.py
@@ -149,6 +149,14 @@ a = random.shuffle(l)
 print(l)
 
 
+# Randomization excercise :
+
+import random
+names = input("Enter everybody's name seperated by comma:")
+names_list =names.split(",")
+length = len(names_list)
+random_choice = random.randint(0,length-1)
+print(f"{names_list[random_choice]} will pay the bill")  
 
 
 
